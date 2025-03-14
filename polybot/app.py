8080fe0ffc6baac8817d45228b976fe103b5aeac
BarRotem <bar.rotem45@gmail.com>
2025-03-14 16:22:16 +0200

    Commit #1 - Debug telegram token

--- polybot/app.py
+++ polybot/app.py
@@ -1,13 +1,47 @@
+# Define required modules
 import flask
 from flask import request
 import os
 from bot import ObjectDetectionBot
 
+import boto3
+from loguru import logger
+
 app = flask.Flask(__name__)
 
 
 # TODO load TELEGRAM_TOKEN value from Secret Manager
-TELEGRAM_TOKEN = ...
+# Use this code snippet in your app.
+# If you need more information about configurations
+# or implementing the sample code, visit the AWS docs:
+# https://aws.amazon.com/developer/language/python/
+
+def get_secret():
+
+    secret_name = "barrotem/polybot/k8s-project"
+    region_name = "eu-north-1"
+
+    # Create a Secrets Manager client
+    session = boto3.session.Session()
+    client = session.client(
+        service_name='secretsmanager',
+        region_name=region_name
+    )
+
+    try:
+        get_secret_value_response = client.get_secret_value(
+            SecretId=secret_name
+        )
+    except ClientError as e:
+        # For a list of exceptions thrown, see
+        # https://docs.aws.amazon.com/secretsmanager/latest/apireference/API_GetSecretValue.html
+        raise e
+    print(get_secret_value_response)
+    secret = get_secret_value_response['SecretString']
+    print(secret)
+
+    # Your code goes here.
+TELEGRAM_TOKEN = get_secret()
 
 TELEGRAM_APP_URL = os.environ['TELEGRAM_APP_URL']
 
